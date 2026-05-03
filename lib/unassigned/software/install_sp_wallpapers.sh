f5cbf5d5e09920980e83a5d925122647da0834ff
souari1974 <139868804+souari1974@users.noreply.github.com>
2026-05-03 19:50:00 +0200

    wallpapers

--- lib/unassigned/software/install_sp_wallpapers.sh
+++ lib/unassigned/software/install_sp_wallpapers.sh
@@ -2,17 +2,6 @@
 # Fleet passes the PKG path via $INSTALLER_PATH
 # This just runs the PKG, which contains the real install logic as postinstall script
 
-set -uo pipefail
-
-LOG="/var/log/sp_wallpapers_install.log"
-
-log() {
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" | tee -a "$LOG"
-}
-
-log "=== S•P Wallpapers install started ==="
-
-
 installer -pkg "$INSTALLER_PATH" -target /
 
 username=$(stat -f%Su /dev/console)
@@ -22,8 +11,8 @@ rm /Users/$username/Library/Preferences/com.apple.systempreferences.plist
 killall -HUP cfprefsd
 
 # Ajouter le chemin des fonds d'écran aux Préférences Système
-defaults write /Library/Preferences/com.apple.systempreferences DSKDesktopPrefPane '<dict><key>UserFolderPaths</key><array><string>/Users/Shared/Wallpapers_SP</string></array></dict>'
-defaults write /Users/$username/Library/Preferences/com.apple.systempreferences DSKDesktopPrefPane '<dict><key>UserFolderPaths</key><array><string>/Users/Shared/Wallpapers_SP</string></array></dict>'
+defaults write /Library/Preferences/com.apple.systempreferences DSKDesktopPrefPane '<dict><key>UserFolderPaths</key><array><string>/Users/Shared/sp_wallpapers</string></array></dict>'
+defaults write /Users/$username/Library/Preferences/com.apple.systempreferences DSKDesktopPrefPane '<dict><key>UserFolderPaths</key><array><string>/Users/Shared/sp_wallpapers</string></array></dict>'
 
 killall -HUP cfprefsd
 killall "System Settings"
