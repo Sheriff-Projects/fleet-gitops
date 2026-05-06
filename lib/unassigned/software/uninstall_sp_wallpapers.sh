12623b96fb09c97d90adf92ca516b6237cc2acdc
souari1974 <139868804+souari1974@users.noreply.github.com>
2026-05-06 11:25:58 +0200

    update

--- lib/unassigned/software/uninstall_sp_wallpapers.sh
+++ lib/unassigned/software/uninstall_sp_wallpapers.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 # Désinstallation de S•P Wallpapers
 
-set -uo pipefail
 
-LOG="/var/log/sp_wallpapers_uninstall.log"
-
-log() {
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" | tee -a "$LOG"
-}
-
-log "=== S•P Wallpapers uninstall started ==="
 
 exit 0
